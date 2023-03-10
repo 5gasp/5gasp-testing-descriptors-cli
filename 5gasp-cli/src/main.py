bf4c821f43eff27c260565f948bea2ecc3f32074
Eduardo Santos <eduardosantoshf@gmail.com>
2023-03-10 17:34:16 +0000

    Connection point tags are now injected on the tests descriptor

--- 5gasp-cli/src/main.py
+++ 5gasp-cli/src/main.py
@@ -2,7 +2,7 @@
 # @Author: Eduardo Santos
 # @Date:   2023-02-01 16:31:36
 # @Last Modified by:   Eduardo Santos
-# @Last Modified time: 2023-03-09 17:48:18
+# @Last Modified time: 2023-03-10 17:17:17
 
 # OS
 import sys
@@ -26,7 +26,7 @@ from Parser.parser import InjectedTagsParser
 app = typer.Typer(pretty_exceptions_show_locals = False)
 state = {"verbose": False}
 
-yaml = YAML(typ = "safe") # safe mode - loads a document without resolving unknown tags
+yaml = YAML()
 yaml.default_flow_style = False
 
 @app.command()
@@ -71,7 +71,8 @@ def create_tests(
             
             print(f"\nInfering tags from the following descriptors: {descriptors}")
 
-        infer_tags(infer_tags_from_nsd)
+        connection_points = infer_tags(infer_tags_from_nsd)
+        connection_point_values = read_connection_point_values()
 
     if state["verbose"]: 
         print("\nReading configuration file...")
@@ -89,11 +90,27 @@ def create_tests(
     if state["verbose"]: 
         print("Configuration file read!")
 
-    descriptor = reset_sections(intended_tests, clear_executions)
+    # testing descriptors from testing-descriptor_nods.yaml
+    tests = read_testing_descriptors()
+
+    # tests info from test_information.yaml
+    tests_info = read_tests_info()
+
+    cleared_tests = reset_sections(tests, clear_executions)
+
+    # add intended tests to testcases
+    descriptor = add_tests_to_testcases(
+                    cleared_tests, 
+                    tests_info, 
+                    intended_tests, 
+                    connection_points,
+                    connection_point_values
+                )
 
     if state["verbose"]: 
         print("Creating the descriptor...")
 
+    # save data to the descriptor
     with open("../../" + output_filename, "w") as file:
         try:
             yaml.dump(descriptor, file)
@@ -101,17 +118,17 @@ def create_tests(
             print(exc)
 
     if state["verbose"]:
-        print("\nDescriptor generated, check it at the descriptor folder.")
+        print("\nDescriptor generated!")
 
 
-def reset_sections(intended_tests: dict(), clear_executions: bool):
+def reset_sections(tests: dict(), clear_executions: bool):
     '''
-    Reset user-given sections to later be filled by the user
+    Reset user-given sections to later be filled by the developer
 
     Parameters
     ----------
-    intended_tests : dict
-        Dictionary containing tests information from test_information.yaml.
+    tests : dict
+        Dictionary containing testing descriptors.
     clear_executions : bool
         Boolean for whether to clear existing executions.
     
@@ -120,40 +137,9 @@ def reset_sections(intended_tests: dict(), clear_executions: bool):
     tests: dict
         Updated testing descriptors.
     '''
-    # tests info from test_information.yaml
-    tests_info = read_tests_info()
-
-    # testing descriptors from testing-descriptor_nods.yaml
-    tests = read_testing_descriptors()
-
-    # existing tests
-    test_types = tests_info['tests']['testbed_itav']
 
     tests['test_phases']['setup']['testcases'].clear()
     
-    intended_test_types = [type for type in test_types 
-                            if type in intended_tests['tests']]
-    
-    # add intended test to testcases
-    for i, test in enumerate(intended_test_types, 1):
-        test = test_types[test]
-        
-        testcase = Testcase(
-                        id = i,
-                        type = test['test_type'],
-                        name = test['name'],
-                        description = test['description'],
-                    )
-
-        testcase.create_testcase()
-
-        # add parameters to testcase
-        for variable in test['test_variables']:
-            testcase.add_parameter({'key': variable['variable_name'], 'value': ""})
-
-        # add testcase to tests
-        tests['test_phases']['setup']['testcases'].append(testcase.testcase)
-        
     if clear_executions:
       
         tests['test_phases']['execution'].clear()
@@ -168,6 +154,102 @@ def reset_sections(intended_tests: dict(), clear_executions: bool):
 
     return tests
 
+def add_tests_to_testcases(
+        tests: dict(), 
+        tests_info: dict(), 
+        intended_tests: dict(),
+        connection_points: dict(),
+        connection_point_values: dict()
+        ):
+    '''
+    Add developer given tests to the testcases
+
+    Parameters
+    ----------
+    tests : dict
+        Dictionary containing testing descriptors.
+    tests_info : dict
+        Dictionary containing tests info.
+    intended_tests : bool
+        Dictionary containing tests to be added.
+    connection_points: dict
+        Connection points infered from NSD(s)
+    connection_point_values: dict
+        Possible values for the connection points.
+    
+    Returns
+    -------
+    tests: dict
+        Updated testing descriptors.
+    '''
+
+    # existing tests
+    test_types = tests_info['tests']['testbed_itav']
+
+    intended_test_types = [type for type in test_types 
+                            if type in intended_tests['tests']]
+    
+    for i, test in enumerate(intended_test_types, 1):
+        test = test_types[test]
+        
+        testcase = Testcase(
+                        id = i,
+                        type = test['test_type'],
+                        name = test['name'],
+                        description = test['description'],
+                    )
+
+        testcase.create_testcase()
+
+        # add parameters to testcase
+        for variable in test['test_variables']:
+            tag = ""
+            
+            if variable['injected_by_nods'] \
+                and variable['injected_artifact_type'] == "connection_point":
+                print("True")
+
+                print(f"\nThe {variable['variable_name']} parameter must have a connection point injected")
+
+                print("\nThe following connection points were infered from the given NSD(s):")
+
+                for i, connection_point in enumerate(connection_points, 1):
+                    print(f"{i}: {connection_point}")
+
+                while True:
+                    cp = input("\nWhich connection point do you want to inject on the parameter? ")
+
+                    if int(cp) not in range(1, i + 1):
+                        print(f"\nERROR! The connection point must be an int between 1 - {i}")
+                        continue
+                    else:
+                        break
+
+                print("\nThe following values can be injected into the connection point:")
+
+                for i, connection_point_value in enumerate(connection_point_values, 1):
+                    print(f"{i}: {connection_point_value}")
+
+                while True:
+                    value = input("\nWhich value do you want to inject on the connection point? ")
+
+                    if int(value) not in range(1, i + 1):
+                        print(f"\nERROR! The value must be an int between 1 - {i}")
+                        continue
+                    else:
+                        break
+                    
+                tag = connection_points[int(cp) - 1][:-2] + "|" \
+                        + connection_point_values[int(value) - 1] + "}}"
+
+            testcase.add_parameter({'key': variable['variable_name'], 'value': tag})
+
+        # add testcase to tests
+        tests['test_phases']['setup']['testcases'].append(testcase.testcase)
+
+    return tests
+
+
 def infer_tags(network_service_descriptor: list()):
     '''
     Infer tags from given NSD(s)
@@ -176,6 +258,11 @@ def infer_tags(network_service_descriptor: list()):
     ----------
     network_service_descriptor : list[str]
         List with NSDs to be used to infer tags 
+
+     Returns
+    -------
+    tags: dict
+        Connection points infered.
     '''
     if not network_service_descriptor:
         nsd = input("Are you sure you want to continue without providing a NSD? [y/n]: ")
@@ -187,11 +274,9 @@ def infer_tags(network_service_descriptor: list()):
             parser = InjectedTagsParser(descriptor)
             parser.parse_descriptor()
 
-            interfaces = parser.interfaces
+            tags = parser.interfaces
 
-            print("\nThe following tags were generated:")
-            for interface in interfaces:
-                print(interface)
+    return tags
 
 
 def read_tests_info():
@@ -236,6 +321,27 @@ def read_testing_descriptors():
         return sys.exit(0)
 
     return testing_descriptor_nods
+
+def read_connection_point_values():
+    '''
+    Read connection point values from connection_point_values.yaml
+    
+    Returns
+    -------
+    connection_point_values : dict
+        Dictionary containing the connection point values.
+    '''
+    try:
+        with open("../../resources/connection_point_values.yaml", "r") as stream:
+            try:
+                connection_point_values = yaml.load(stream)
+            except YAMLError as exc:
+                print(exc)
+    except FileNotFoundError as e:
+        print(f"Error! File connection_point_values.yaml not found!")
+        return sys.exit(0)
+
+    return connection_point_values['values']
      
 
 @app.callback()
